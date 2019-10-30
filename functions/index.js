ffba38b89f2db6f66545eea3d2f693cb425a503f
Emily Evans <emilyevans@wifi-roaming-128-4-110-127.host.udel.edu>
2019-10-30 11:18:22 -0400

    everything should be up to date and set up on firebase

--- functions/index.js
+++ functions/index.js
@@ -3,16 +3,6 @@ const functions = require('firebase-functions');
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello World!");
-});
-
-
-var admin = require("firebase-admin");
-
-var serviceAccount = require("path/to/serviceAccountKey.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://diyreddit-7f814.firebaseio.com"
-});
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
