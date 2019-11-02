8c87a60f9ad21494db5082ec7206b3be98a5c4b8
Emily Evans <emilyevans@wifi-roaming-128-4-74-92.host.udel.edu>
2019-11-02 16:56:52 -0400

    trying to get it to show up correctly

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,18 @@
 // The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 const functions = require('firebase-functions');
-var provider = new firebase.auth.GithubAuthProvider();
+// var provider = new firebase.auth.GithubAuthProvider();
 // The Firebase Admin SDK to access the Firebase Realtime Database.
-const admin = require('firebase-admin');
-admin.initializeApp();
+// const admin = require('firebase-admin');
+// admin.initializeApp();
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
 // setup materialize components
 document.addEventListener('DOMContentLoaded', function() {
-
 	var modals = document.querySelectorAll('.modal');
 	M.Modal.init(modals);
   
 	var items = document.querySelectorAll('.collapsible');
 	M.Collapsible.init(items);
   
-  });
+});
