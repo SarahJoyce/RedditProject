7d54ba91d7fb889ab6e532d97b085b4f83356cc1
Emily Evans <emilyevans@wifi-roaming-128-4-74-92.host.udel.edu>
2019-11-02 16:07:10 -0400

    reformatted things

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,5 @@
 // The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 const functions = require('firebase-functions');
-const auth = firebase.auth();
-const db = firebase.firestore();
-
 var provider = new firebase.auth.GithubAuthProvider();
 // The Firebase Admin SDK to access the Firebase Realtime Database.
 const admin = require('firebase-admin');
@@ -10,10 +7,6 @@ const admin = require('firebase-admin');
 admin.initializeApp();
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
-
-exports.helloWorld = functions.https.onRequest((request, response) => {
-	response.send("Hello World!");
-});
 
 // setup materialize components
 document.addEventListener('DOMContentLoaded', function() {
