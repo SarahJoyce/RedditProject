2d7331cc400a6bf4c0e32adbc8a6fb4bc03b6604
Emily Evans <emilyevans@wifi-roaming-128-4-74-92.host.udel.edu>
2019-11-02 15:43:31 -0400

    the most recent changes I added in the sdk for the project in init-firebase and am updating js and html

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
 // The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 const functions = require('firebase-functions');
 const auth = firebase.auth();
-var provider = new firebase.auth.GithubAuthProvider(); 
+var provider = new firebase.auth.GithubAuthProvider();
 // The Firebase Admin SDK to access the Firebase Realtime Database.
 const admin = require('firebase-admin');
 admin.initializeApp();
@@ -9,34 +9,53 @@ admin.initializeApp();
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello World!");
+	response.send("Hello World!");
 });
-
+/* 
 firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
 	// Handle Errors here.
 	var errorCode = error.code;
 	var errorMessage = error.message;
-	// ...
-  });email-password.html
+	// [START_EXCLUDE]
+	if (errorCode == 'auth/weak-password') {
+	  alert('The password is too weak.');
+	} else {
+	  alert(errorMessage);
+	}
+	console.log(error);
+	// [END_EXCLUDE]
+  });
+  // [END createwithemail]
 
-firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
 	// Handle Errors here.
 	var errorCode = error.code;
 	var errorMessage = error.message;
-	// ...
-  });email-password.html
+	// [START_EXCLUDE]
+	if (errorCode === 'auth/wrong-password') {
+		alert('Wrong password.');
+	} else {
+		alert(errorMessage);
+	}
+	console.log(error);
+	document.getElementById('quickstart-sign-in').disabled = false;
+	// [END_EXCLUDE]
+});
+// [END authwithemail]
+document.getElementById('quickstart-sign-in').disabled = true;
+
 // Callback fires off any time authentication state changes
 // user logs in firebaseUser will be populated with firebase user 
 // if user logs out fill with NULL
-auth.onAuthStateChanged(firebaseUser => {});
+auth.onAuthStateChanged(firebaseUser => { });
 
-firebase.auth().signInWithPopup(provider).then(function(result) {
+firebase.auth().signInWithPopup(provider).then(function (result) {
 	// This gives you a GitHub Access Token. You can use it to access the GitHub API.
 	var token = result.credential.accessToken;
 	// The signed-in user info.
 	var user = result.user;
 	// ...
-  }).catch(function(error) {
+}).catch(function (error) {
 	// Handle Errors here.
 	var errorCode = error.code;
 	var errorMessage = error.message;
@@ -45,10 +64,21 @@ firebase.auth().signInWithPopup(provider).then(function(result) {
 	// The firebase.auth.AuthCredential type that was used.
 	var credential = error.credential;
 	// ...
-  });
+});
 
-  firebase.auth().signOut().then(function() {
+firebase.auth().signOut().then(function () {
 	// Sign-out successful.
-  }).catch(function(error) {
+}).catch(function (error) {
 	// An error happened.
+});
+ */
+// setup materialize components
+document.addEventListener('DOMContentLoaded', function() {
+
+	var modals = document.querySelectorAll('.modal');
+	M.Modal.init(modals);
+  
+	var items = document.querySelectorAll('.collapsible');
+	M.Collapsible.init(items);
+  
   });
--- init-firebase.js
+++ init-firebase.js
@@ -1,7 +1,13 @@
-// TODO: Replace the following with your app's Firebase project configuration
 var firebaseConfig = {
-	// ...
+    apiKey: "AIzaSyDUzNRAHGQsPRRFkPL3P0rdpplGuPbRA5M",
+    authDomain: "diyreddit-7f814.firebaseapp.com",
+    databaseURL: "https://diyreddit-7f814.firebaseio.com",
+    projectId: "diyreddit-7f814",
+    storageBucket: "diyreddit-7f814.appspot.com",
+    messagingSenderId: "401162339207",
+    appId: "1:401162339207:web:a785264f490353e9b34922",
+    measurementId: "G-ESK47Z7DWY"
   };
-  
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
