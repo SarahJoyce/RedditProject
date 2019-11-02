4d53675d3fda19a982412aa96b5212fd7a968e11
Emily Evans <emilyevans@wifi-roaming-128-4-74-92.host.udel.edu>
2019-11-02 16:02:53 -0400

    hi

--- functions/index.js
+++ functions/index.js
@@ -8,9 +8,9 @@ var provider = new firebase.auth.GithubAuthProvider();
 const admin = require('firebase-admin');
 
 admin.initializeApp();
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+
 exports.helloWorld = functions.https.onRequest((request, response) => {
 	response.send("Hello World!");
 });
